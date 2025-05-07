f64cf0f55a1a6d50058d9a762324006006073ec3
Juan <juangallo001@gmail.com>
2025-05-07 09:05:52 -0300

    Arreglo de rutas

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,17 +1,17 @@
 const navElements=[
-  {title:'Ropa de Hombre',link:'../../pages/categorias/RopaHombre.html'},
-  {title:'Ropa de Mujer',link:'../../pages/categorias/RopaMujer.html'},
-  {title:'Electrónica',link:'../../pages/categorias/Electronica.html'},
-  {title:'<img src="../../imgs/carrito.png" alt="Carrito" width="40" height="40" class="rounded-circle img-carrito">',link:"../../pages/carrito/carrito.html"},
-  {title:'Cerrar Sesion',link:'../../pages/inicio/login.html'}
+  {title:'Ropa de Hombre',link:'/pages/categorias/RopaHombre.html'},
+  {title:'Ropa de Mujer',link:'/pages/categorias/RopaMujer.html'},
+  {title:'Electrónica',link:'/pages/categorias/Electronica.html'},
+  {title:'<img src="/imgs/carrito.png" alt="Carrito" width="40" height="40" class="rounded-circle img-carrito">',link:"/pages/carrito/carrito.html"},
+  {title:'Cerrar Sesion',link:'/pages/inicio/login.html'}
 ]
 
 
 export const NavBar=`
  <nav class="navbar navbar-expand-lg bg-card">
         <div class="container-fluid">
-          <a class="navbar-brand"  href="../../index.html">
-            <img src="../../imgs/logo.png" alt="Logo" width="75" height="75" class="rounded-circle bg-logo">
+          <a class="navbar-brand"  href="/index.html">
+            <img src="/imgs/logo.png" alt="Logo" width="75" height="75" class="rounded-circle bg-logo">
           </a>
           <button class="navbar-toggler bg-nav-button" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
--- pages/categorias/categoriasController.js
+++ pages/categorias/categoriasController.js
@@ -1,5 +1,5 @@
-import { NavBar } from "../../components/NavBar.js"
-import { cardComponents } from "../../components/card.js"
+import { NavBar } from "/components/NavBar.js"
+import { cardComponents } from "/components/card.js"
 
 
 let cardContainer=document.getElementById('card-container')
